a4b83f969a31fd787f9a095bbc43e6f0af463218
JuanMa1215 <jmanuel.sepulveda1@udea.edu.co>
2022-01-26 15:30:35 -0500

    Problema_2

---
+++ Problema_2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+   char v[200];
+   int i;
+   int p;
+   char c = 65;
+   int k = 0;
+   int e;
+   srand(time(NULL));
+   for(i=0;i<200;i++){
+       v[i] = char(65+rand()%(90-65+1));
+       cout<< v[i];
+   }
+   for(e=0;e<26;e++){
+        for(p=0;p<200;p++){
+            if( c == v[p]){
+                k++;
+             }
+        }
+        cout<<"\n"<<c<<": "<<k;
+        k=0;
+        c++;
+   }
+
+
+
+}
